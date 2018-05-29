f7c7007b5cef7d2b49c476298f6408525a2e346d
Michael Edwards <michael.edwards@jamstockex.com>
2018-05-29 10:50:04 -0500

    commit existing files

---
+++ TARSReport/Default.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="TARSReport._Default" %>
+
+<%@ Register assembly="CrystalDecisions.Web, Version=13.0.3500.0, Culture=neutral, PublicKeyToken=692fbea5521e1304" namespace="CrystalDecisions.Web" tagprefix="CR" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        <p class="lead">TARS Reports</p>
+    </div>
+
+    <div class="row">
+        <div class="col-md-4">
+            <h2>
+                <asp:DropDownList ID="DropDownList1" runat="server" DataSourceID="SqlDataSource1" DataTextField="DES" DataValueField="Location">
+                </asp:DropDownList>
+            </h2>
+            <p>
+                &nbsp;<asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:WebFeedRepositoryConnectionString %>" SelectCommand="SELECT ID, LTRIM(RTRIM(LOC)) + LTRIM(RTRIM(FIL)) AS Location, DES FROM REPORT WHERE VISIBLE='Y'"></asp:SqlDataSource>
+            </p>
+        </div>
+        <div class="col-md-4">
+            <h2>
+                <asp:Button ID="GenerateReport" runat="server" OnClick="GenerateReport_Click" Text="ViewReport" />
+            </h2>
+            <CR:CrystalReportViewer ID="CrystalReportViewer1" runat="server" AutoDataBind="true" EnableDatabaseLogonPrompt="False" />
+            <p>
+            </p>
+        </div>
+    </div>
+
+</asp:Content>
---
+++ TARSReport/Default.aspx.cs
@@ -0,0 +1,39 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using System;
+using System.Linq;
+using System.Web.UI;
+
+namespace TARSReport
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            CrystalReportViewer1.ReportSource = (ReportDocument)Session["myReport"];
+        }
+
+        protected void GenerateReport_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ReportDocument reports = new ReportDocument();
+                reports.Load(DropDownList1.SelectedValue.ToString());
+                if (DropDownList1.SelectedValue.ToString().Contains("CESS") || DropDownList1.SelectedValue.ToString().Contains("CA") || DropDownList1.SelectedValue.ToString().Contains("Dividend"))
+                {
+                    reports.SetDatabaseLogon(Properties.Settings.Default.UserName, Properties.Settings.Default.Password, Properties.Settings.Default.Server, Properties.Settings.Default.DatabaseWebFeedRepository);
+                }
+                else
+                {
+                    reports.SetDatabaseLogon(Properties.Settings.Default.UserName, Properties.Settings.Default.Password, Properties.Settings.Default.Server, Properties.Settings.Default.DatabaseWebRepository);
+                }
+                CrystalReportViewer1.Visible = true;
+                CrystalReportViewer1.ReportSource = reports;
+                Session["myReport"] = reports;
+            }
+            catch (Exception exc)
+            {
+                throw exc;
+            }
+        }
+    }
+}
